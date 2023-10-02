2f669f5b3ae4a894b1913ee5d088e007814433be
programador <rkhut@h2plt.energy>
2023-10-02 13:50:30 +0200

    add: payment type spinner in order content

--- app/src/main/java/ua/com/programmer/agentventa/documents/order/OrderPageContent.kt
+++ app/src/main/java/ua/com/programmer/agentventa/documents/order/OrderPageContent.kt
@@ -4,19 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import dagger.hilt.android.AndroidEntryPoint
+import ua.com.programmer.agentventa.R
 import ua.com.programmer.agentventa.databinding.ModelContentOrderGoodsBinding
 import ua.com.programmer.agentventa.extensions.format
+import ua.com.programmer.agentventa.shared.SharedViewModel
 
 @AndroidEntryPoint
 class OrderPageContent: Fragment() {
 
     private val viewModel: OrderViewModel by activityViewModels()
+    private val sharedModel: SharedViewModel by activityViewModels()
     private var _binding: ModelContentOrderGoodsBinding? = null
     private val binding get() = _binding!!
     private lateinit var recyclerView: RecyclerView
@@ -28,6 +33,9 @@ class OrderPageContent: Fragment() {
     ): View {
         _binding = ModelContentOrderGoodsBinding.inflate(inflater,container,false)
         binding.lifecycleOwner = viewLifecycleOwner
+
+        setupPaymentSpinner()
+
         return binding.root
     }
 
@@ -54,10 +62,36 @@ class OrderPageContent: Fragment() {
             binding.apply {
                 orderTotalPrice.text = it.price.format(2)
                 orderTotalWeight.text = it.weight.format(3)
+                docPaymentType.isEnabled = it.isProcessed == 0
             }
             adapter.setClickable(it.isProcessed == 0)
+            setSelectedPaymentType(it.paymentType)
         }
 
+    }
+
+    private fun setSelectedPaymentType(value: String) {
+        val position = sharedModel.paymentTypes.indexOfFirst { it.paymentType == value }
+        binding.docPaymentType.setSelection(position)
+    }
+
+    private fun setupPaymentSpinner() {
+
+        val spinnerList = sharedModel.paymentTypes.map { it.description }
+        val adapter = ArrayAdapter(requireContext(), R.layout.price_spinner_item, spinnerList)
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item)
+        binding.docPaymentType.adapter = adapter
+
+        binding.docPaymentType.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val type = sharedModel.getPaymentType(spinnerList[position])
+                viewModel.onPaymentTypeSelected(type)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Another interface callback
+            }
+        }
     }
 
     override fun onDestroyView() {
