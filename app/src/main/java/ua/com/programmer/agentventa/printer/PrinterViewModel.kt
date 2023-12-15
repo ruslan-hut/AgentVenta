47b8a7375896024cf18653d900e0ab6600d26aa2
ruslanhut <dev@programmer.com.ua>
2023-12-15 23:32:16 +0100

    changed printing test

--- app/src/main/java/ua/com/programmer/agentventa/printer/PrinterViewModel.kt
+++ app/src/main/java/ua/com/programmer/agentventa/printer/PrinterViewModel.kt
@@ -11,6 +11,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import ua.com.programmer.agentventa.BuildConfig
 import java.io.IOException
 import java.util.UUID
 import javax.inject.Inject
@@ -64,32 +65,43 @@ class PrinterViewModel @Inject constructor(
                 outputStream.write(byteArrayOf(0x1B, 0x40)) // ESC @
 
                 // Print a regular line of text
-                outputStream.write("Regular text line\n".toByteArray())
+//                outputStream.write("Regular text line\n".toByteArray())
 
                 // Bold text
-                outputStream.write(byteArrayOf(0x1B, 0x45, 0x1))  // ESC E n
-                outputStream.write("Bold text line\n".toByteArray())
-                outputStream.write(byteArrayOf(0x1B, 0x45, 0x0))  // Cancel bold
+//                outputStream.write(byteArrayOf(0x1B, 0x45, 0x1))  // ESC E n
+//                outputStream.write("Bold text line\n".toByteArray())
+//                outputStream.write(byteArrayOf(0x1B, 0x45, 0x0))  // Cancel bold
 
                 // Double height
-                outputStream.write(byteArrayOf(0x1D, 0x21, 0x01)) // GS ! n
-                outputStream.write("Double height text line\n".toByteArray())
-                outputStream.write(byteArrayOf(0x1D, 0x21, 0x00)) // Cancel double height
+//                outputStream.write(byteArrayOf(0x1D, 0x21, 0x01)) // GS ! n
+//                outputStream.write("Double height text line\n".toByteArray())
+//                outputStream.write(byteArrayOf(0x1D, 0x21, 0x00)) // Cancel double height
 
                 // Center Align
                 outputStream.write(byteArrayOf(0x1B, 0x61, 0x1))  // ESC a n
-                outputStream.write("Centered text\n".toByteArray())
 
-                // Left Align
-                outputStream.write(byteArrayOf(0x1B, 0x61, 0x0))  // ESC a n
-                outputStream.write("Left aligned text\n".toByteArray())
+                // Double height
+                outputStream.write(byteArrayOf(0x1D, 0x21, 0x01)) // GS ! n
+                outputStream.write("Agent Venta\n".toByteArray())
+                outputStream.write(byteArrayOf(0x1D, 0x21, 0x00)) // Cancel double height
 
-                // Right Align
-                outputStream.write(byteArrayOf(0x1B, 0x61, 0x2))  // ESC a n
-                outputStream.write("Right aligned text\n".toByteArray())
+                // Bold text
+                outputStream.write(byteArrayOf(0x1B, 0x45, 0x1))
+                outputStream.write("${BuildConfig.VERSION_NAME}\n".toByteArray())
+                outputStream.write(byteArrayOf(0x1B, 0x45, 0x0))  // Cancel bold
 
+                outputStream.write("printing test\n".toByteArray())
                 // Reset to left align (if necessary)
                 outputStream.write(byteArrayOf(0x1B, 0x61, 0x0))
+                outputStream.write("123456789*123456789*123456789*123456789*\n".toByteArray())
+
+//                // Left Align
+//                outputStream.write(byteArrayOf(0x1B, 0x61, 0x0))  // ESC a n
+//                outputStream.write("Left aligned text\n".toByteArray())
+//
+//                // Right Align
+//                outputStream.write(byteArrayOf(0x1B, 0x61, 0x2))  // ESC a n
+//                outputStream.write("Right aligned text\n".toByteArray())
 
                 // Feed and Cut
                 outputStream.write(byteArrayOf(0x0A))  // Print and line feed
