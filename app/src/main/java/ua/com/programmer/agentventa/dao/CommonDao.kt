02810fef30444941b7389cdfffab59b2b4a938c7
ruslanhut <dev@programmer.com.ua>
2024-11-20 13:10:30 +0100

    fix: order content deletion

--- app/src/main/java/ua/com/programmer/agentventa/dao/CommonDao.kt
+++ app/src/main/java/ua/com/programmer/agentventa/dao/CommonDao.kt
@@ -10,7 +10,7 @@ interface CommonDao {
     suspend fun deleteOrders(from: Long): Int
 
     @Query("DELETE FROM order_content " +
-            "WHERE order_guid IN (SELECT guid FROM orders WHERE time > :from)")
+            "WHERE order_guid IN (SELECT guid FROM orders WHERE time < :from)")
     suspend fun deleteOrderContent(from: Long): Int
 
     @Query("DELETE FROM cash WHERE time < :from")
