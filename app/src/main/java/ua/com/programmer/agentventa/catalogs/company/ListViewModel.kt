ccc3b7215dac2ab77ee03aa64ce0441a4f57f004
el hombre <ruslan.hoot@icloud.com>
2025-05-16 11:46:04 +0200

    removed unused import

--- app/src/main/java/ua/com/programmer/agentventa/catalogs/company/ListViewModel.kt
+++ app/src/main/java/ua/com/programmer/agentventa/catalogs/company/ListViewModel.kt
@@ -1,7 +1,5 @@
 package ua.com.programmer.agentventa.catalogs.company
 
-import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
