c51d6d51cea58359b3d9394e55ed589c9f996f0b
ruslanhut <dev@programmer.com.ua>
2025-12-22 20:29:01 +0100

    fix: resource provider refactoring

--- app/src/main/java/ua/com/programmer/agentventa/utility/ResourceProviderImpl.kt
+++ app/src/main/java/ua/com/programmer/agentventa/utility/ResourceProviderImpl.kt
@@ -11,7 +11,7 @@ import javax.inject.Singleton
  */
 @Singleton
 class ResourceProviderImpl @Inject constructor(
-    @ApplicationContext private val context: Context
+    @param:ApplicationContext private val context: Context
 ) : ResourceProvider {
 
     override fun getString(resId: Int): String {
