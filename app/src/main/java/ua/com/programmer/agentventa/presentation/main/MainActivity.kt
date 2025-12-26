6c040b3710f22d384fa0f90208d13f7df6615320
ruslanhut <dev@programmer.com.ua>
2025-12-26 21:32:48 +0100

    unused import

--- app/src/main/java/ua/com/programmer/agentventa/presentation/main/MainActivity.kt
+++ app/src/main/java/ua/com/programmer/agentventa/presentation/main/MainActivity.kt
@@ -3,7 +3,6 @@ package ua.com.programmer.agentventa.presentation.main
 import android.Manifest
 import android.content.ClipData
 import android.content.ClipboardManager
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
